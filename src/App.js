1e4f887369ae2188166021407bba811f85e0ee6e
touqeerbhatti <badsha14072@gmail.com>
2022-09-28 18:33:30 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 
 
-import React from "react";
+import React, { useState } from "react";
 import {Data} from "./Alldata";
 import Foam from "./Foam";
 
 const App=()=>
 {
 
+  const[color,setcolor]=useState("Click me");
+  const[heading,setheading]=useState("initialvalue");
+  
+
   return(
     <>
     <h1>
-      code
+      code App
     </h1>
 
-    <Foam Alldata={Data} />
-    
+    <Foam 
+    Alldata={Data} 
+    color={color}
+    setcolor={setcolor}
+    heading={heading}
+    setheading={setheading}
+
+    />
+
     </>
   )
 }
--- src/Foam.js
+++ src/Foam.js
@@ -5,21 +5,27 @@ import React from "react";
 
 const Foam=props=>
 {   
-    const {Alldata}=props
+    const {Alldata,color,setcolor,heading,setheading}=props
 
     console.log(Alldata)
+
     return(
         <>
-        <h1>
-            Form
+        <h1 onClick={()=>setheading("child")}>
+            {heading}
         </h1>
+        
+        <button onClick={()=>setcolor("Submit me")}>
+        {color}
+        </button>
 
         { Alldata.map((element)=>{
             return(
         <>
 
             <label>Username</label>
-            <input type={element.type} name={element.name} value={element.value} />
+            <input type={element.type} name={element.name} value={element.value}  />
+
         </>
             )
         })
